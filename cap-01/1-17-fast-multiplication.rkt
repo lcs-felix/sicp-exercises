ffea94f0d27877fa1ba8548ea3f354cd1bc2fd4c
Lucas Félix <lfelixsampaio@gmail.com>
2020-06-29 20:38:23 -0300

    1.17 add coment for display a and b values

--- cap-01/1-17-fast-multiplication.rkt
+++ cap-01/1-17-fast-multiplication.rkt
@@ -7,12 +7,14 @@
   (/ n 2))
 
 (define (* a b)
+    ; (display (string-append-immutable "a: " (~a a) ", b: " (~a b)))(newline)
     (cond [(= b 0) 0]
           [(= b 1) a]
           [(odd? b) (+ a (* a (- b 1)))]
-          [else (double (* a (halve b)))]))
+          [else (double a (* a (halve b)))]))
 
-(* 3 4)
-(* 3 6)
-(* 3 7)
+; (* 3 4)
+; (* 3 6)
+; (* 3 7)
 (* 10 10)
+(* 2 5)

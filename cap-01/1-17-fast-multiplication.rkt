b3b0d5280950f6a1a9c59ab4a08f048e86653259
Lucas Félix <lfelixsampaio@gmail.com>
2020-06-29 20:43:50 -0300

    (enter! (file /home/lucas/workspace/sicp-exercises/cap-01/1-17-fast-multiplication.rkt))

--- cap-01/1-17-fast-multiplication.rkt
+++ cap-01/1-17-fast-multiplication.rkt
@@ -9,9 +9,8 @@
 (define (* a b)
     ; (display (string-append-immutable "a: " (~a a) ", b: " (~a b)))(newline)
     (cond [(= b 0) 0]
-          [(= b 1) a]
           [(odd? b) (+ a (* a (- b 1)))]
-          [else (double a (* a (halve b)))]))
+          [else (double (* a (halve b)))]))
 
 ; (* 3 4)
 ; (* 3 6)

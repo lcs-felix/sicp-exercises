c25e6b992756dbd1cc47055130ad5f6dc5930f97
Lucas Félix <lfelixsampaio@gmail.com>
2020-06-29 21:57:48 -0300

    1.18 multiplication iterative

--- cap-01/1-17-1-18-fast-multiplication.rkt
+++ cap-01/1-17-1-18-fast-multiplication.rkt
@@ -6,11 +6,18 @@
 (define (halve n)
   (/ n 2))
 
-(define (* a b)
+(define (mult-recur a b)
     ; (display (string-append-immutable "a: " (~a a) ", b: " (~a b)))(newline)
     (cond [(= b 0) 0]
           [(odd? b) (+ a (* a (- b 1)))]
           [else (double (* a (halve b)))]))
+
+(define (* a b)
+  (define (mult-iter a b acc)
+    (cond [(= b 0) acc]
+          [(even? b) (mult-iter (double a) (halve b) acc)]
+          [else (mult-iter a (- b 1) (+ a acc))]))
+  (mult-iter a b 0))
 
 (* 3 4)
 (* 3 6)

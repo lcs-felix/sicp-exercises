93c8565b4b6c52f0c283d837da0f82c49008209d
Lucas Félix <lfelixsampaio@gmail.com>
2020-08-06 07:56:30 -0300

    2.1 - improving

--- cap-02/rational-numbers.rkt
+++ cap-02/rational-numbers.rkt
@@ -4,13 +4,11 @@
 
 (define (normalize-sign rat)
   (cond 
-    [(and (> (car rat) 0) (> (cdr rat) 0)) rat]
-    [(and (< (car rat) 0) (< (cdr rat) 0)) (cons (abs (car rat)) (abs (cdr rat)))]
-    [(< (car rat) 0) rat]
-    [else (cons (* (car rat) -1) (abs (cdr rat)))]))
+    [(and (negative? (car rat)) (negative? (cdr rat))) (cons (abs (car rat)) (abs (cdr rat)))]
+    [(negative? (cdr rat)) (cons (* (car rat) -1) (abs (cdr rat)))]
+    [else rat]))
 
 (define (make-rat x y)
-  (cond [(and (> x 0) (> y 0))   ])
   (let ([gdc-num (gdc x y)])
     (normalize-sign 
       (cons 

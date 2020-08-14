21b5ac8fc3079bf0478657e51ef949fb70858560
Lucas Félix <lfelixsampaio@gmail.com>
2020-08-13 22:20:47 -0300

    2.18 finish reverse

--- cap-02/2-18-reverse-list.rkt
+++ cap-02/2-18-reverse-list.rkt
@@ -1,9 +1,12 @@
 #lang racket
 
-(define (reverse list)
-  (if (null? (cdr list))
-    (car list)
-    (cons (reverse (cdr list)) (car list))))
+(define (reverse1 original reversed)
+  (if (null? original)
+    reversed
+    (reverse1 (cdr original) (cons (car original) reversed))))
+
+(define (reverse items)
+  (reverse1 items '()))
 
 (define reversed (reverse (list 1 4 9 16 25)))
 (display reversed)

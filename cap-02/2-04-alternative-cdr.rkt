23ee3c96a5b48f9984a3ca5b8a324c5d6265aa45
Lucas Félix <lfelixsampaio@gmail.com>
2020-08-14 05:28:50 -0300

    2.04 alternative implementation for cdr

---
+++ cap-02/2-04-alternative-cdr.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(define pair (cons 1 2))
+(car pair)
+(cdr pair)

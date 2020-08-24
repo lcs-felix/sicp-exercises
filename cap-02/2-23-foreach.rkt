99789aeb2542f4a77c40c170695834c5b334c5fd
Lucas Félix <lfelixsampaio@gmail.com>
2020-08-24 07:48:39 -0300

    2.23 for-each

---
+++ cap-02/2-23-foreach.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(define (for-each func col)
+  (if (null? col)
+    true
+    ((lambda () 
+      (func (car col))
+      (for-each func (cdr col))))))
+
+(for-each (lambda (x)
+  (newline)
+  (display x))
+  (list 57 321 88))

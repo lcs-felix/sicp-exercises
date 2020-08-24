bd9e092043c0f5a96f5ddb7a8430984ac99fdc4a
Lucas Felix <lfelixsampaio@gmail.com>
2020-08-24 17:55:00 -0300

    2.28 fringe

---
+++ cap-02/2-28-fringe.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+
+
+(define (fringe col)
+  (cond [(null? col) '()]
+        [(not (pair? (car col))) (cons (car col) (fringe (cdr col)))]
+        [else (append (fringe (car col))
+                    (fringe (cdr col)))]))
+
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x) ;; (1 2 3 4)
+(fringe (list x x)) ;; (1 2 3 4 1 2 3 4)

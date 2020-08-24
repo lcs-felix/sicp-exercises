55a690d2caac823c7226e6a9cf8c241ec1555105
Lucas Félix <lfelixsampaio@gmail.com>
2020-08-24 20:34:31 -0300

    2.28 fringe

--- cap-02/2-28-fringe.rkt
+++ cap-02/2-28-fringe.rkt
@@ -10,3 +10,7 @@
 (define x (list (list 1 2) (list 3 4)))
 (fringe x) ;; (1 2 3 4)
 (fringe (list x x)) ;; (1 2 3 4 1 2 3 4)
+
+;; just testing...
+(fringe (list 1 (list (list 2 (list 3)))))
+(fringe (list 1 (list 2 (list 4 5) 3)))

2654a1a02117e6a8bbe593b3a9e5cb127c5556b5
Lucas Félix <lfelixsampaio@gmail.com>
2020-12-30 09:30:55 -0300

    improve equal?

--- scheme/cap-02/2-54-equal?.rkt
+++ scheme/cap-02/2-54-equal?.rkt
@@ -4,19 +4,16 @@
   (cond 
     [(null? (cdr l1)) (null? (cdr l2))]
     [(null? (cdr l2)) false]
-    [(symbol? (car l1))
-      (if (symbol? (car l2))
-        (and 
-          (eq? (car l1) (car l2)) 
-          (equal? (cdr l1) (cdr l2)))
-        false)]
+    [(symbol? (car l1)) 
+      (and (eq? (car l1) (car l2))
+            (equal? (cdr l1) (cdr l2)))]
     [else (and 
             (equal? (car l1) (car l2))
             (equal? (cdr l1) (cdr l2)))]))
 
-(equal? '(this is a list) '(this is a list))
-(equal? '(this is a list) '(this is a))
-(equal? '(this is a) '(this is a list))
-(equal? '(this is a list) '(this (is a) list))
-(equal? '(this (is a) list) '(this (is a) list))
-(equal? '(this (is (a)) list) '(this (is (a)) list))
+(eq? (equal? '(this is a list) '(this is a list)) #t)
+(eq? (equal? '(this is a list) '(this is a)) #f)
+(eq? (equal? '(this is a) '(this is a list)) #f)
+(eq? (equal? '(this is a list) '(this (is a) list)) #f)
+(eq? (equal? '(this (is a) list) '(this (is a) list)) #t)
+(eq? (equal? '(this (is (a)) list) '(this (is (a)) list)) #t)

ce57ae9005cd999653155c924d623a8964dcc52d
Lucas Félix <lfelixsampaio@gmail.com>
2020-12-30 15:55:35 -0300

    union on ordered set

--- scheme/cap-02/2.61-2.62-ordered-sets.rkt
+++ scheme/cap-02/2.61-2.62-ordered-sets.rkt
@@ -21,13 +21,24 @@
 ;; 2.62
 
 ;; not O(n)
+;; (define (union-set set1 set2)
+;;  (if (null? set1) 
+;;      set2
+;;      (adjoin-set (car set1) (union-set (cdr set1) set2))))
+
+;; solution from http://community.schemewiki.org/?sicp-ex-2.62
+
 (define (union-set set1 set2)
-  (if (null? set1) 
-      set2
-      (adjoin-set (car set1) (union-set (cdr set1) set2))))
+  (cond [(null? set1) set2]
+        [(null? set2) set1]
+        [else (let ([x1 (car set1)]
+                    [x2 (car set2)])
+                (cond [(= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2)))]
+                      [(< x1 x2) (cons x1 (union-set (cdr set1) set2))]
+                      [else (cons x2 (union-set set1 (cdr set2)))]))]))
 
 (union-set '(1 2) '(3 4))
-(union-set '(4 3 5) '(3 4))
+(union-set '(3 4 5) '(1 2 3 4))
 (union-set '(1 2) '(3 4 5 6))
 (union-set '(1 2) '(1 3 4 5 6))
 (union-set '(10 11) '(1 3))
